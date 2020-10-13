61967d3516c45c231b34469506ab7ee045833c66
= <douglas.lisboa@edu.unipar.br>
2020-10-13 16:01:26 -0300

    Implementando style da rota de mapa; Adicionado style da rota de mapa;

--- src/pages/OrphanagesMap.tsx
+++ src/pages/OrphanagesMap.tsx
@@ -1,8 +1,34 @@
 import React from 'react';
 
+import { FiPlus } from 'react-icons/fi';
+import { Link } from 'react-router-dom';
+
+import '../styles/pages/orphanages-map.css';
+import mapMarkerImg from '../img/Logo.svg';
+
 function OrphanagesMap() {
     return(
-        <h1>Hello Peoople</h1>
+        <div id='page-map'>
+            <aside>
+                <header>
+                    <img src={mapMarkerImg} alt="Logo"/>
+
+                    <h2>Escolha um orfanato no mapa</h2>
+                    <p>Muitas crianças estão esperando a sua visita :)</p>
+                </header>
+
+                <footer>
+                    <strong>Paraná</strong>
+                    <span>Paranavaí</span>
+                </footer>
+            </aside>
+
+            <div></div>
+
+            <Link to='' className='create-orphanage'>
+                <FiPlus size={32} color='#FFF'/>
+            </Link>
+        </div>
     );
 }
 
