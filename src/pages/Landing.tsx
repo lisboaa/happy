0ca406aff7443a0c3eb8e88cb6ab20c5de1dbfe4
= <douglas.lisboa@edu.unipar.br>
2020-10-16 18:01:44 -0300

    Refactoração

--- src/pages/Landing.tsx
+++ src/pages/Landing.tsx
@@ -3,7 +3,6 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import { FiArrowRight } from 'react-icons/fi'
 import '../styles/pages/landing.css';
-import ilustrate from '../img/Ilustra02.svg';
 import logo from '../img/Logo.svg';
 
 function Landin() {
