c4884dc4467c742cd5479ca3f818ebb793302b84
= <douglas.lisboa@edu.unipar.br>
2020-10-17 18:19:56 -0300

    Adicionado espaço

--- src/pages/OrphanagesMap.tsx
+++ src/pages/OrphanagesMap.tsx
@@ -57,6 +57,7 @@ function OrphanagesMap() {
                     <TileLayer
                         url={`https://api.mapbox.com/styles/v1/mapbox/dark-v10/tiles/256/{z}/{x}/{y}@2x?access_token=${process.env.REACT_APP_MAPBOX_TOKEN}`}
                     />
+                    
                     {orphanages.map( orphanage => {
                         
                         return(
